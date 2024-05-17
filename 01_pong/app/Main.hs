6f1efaa59dfae5d445c45adcbbaea4611b7b9460
Brendan DeLeeuw <test>
2024-05-16 20:35:29 -0600

    Basic input
    Added a basic input system that uses hold instead of switch
    
    Not sure I like this implementation but I'm glad I got it working
    
    On one hand the fact that the lower level functions operate on direct types instead of Event types feels better, but I know that they are really acting on a stream of the type instead of a stream of the events of those tyeps, which feels not so good

--- 01_pong/app/Main.hs
+++ 01_pong/app/Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE Arrows #-}
 
 import Control.Concurrent
-import Control.Arrow                      ( returnA, (>>>) )
+import Control.Arrow                      ( returnA, (>>>), arr )
 import FRP.Yampa                          ( SF, Event (Event)
-                                          , integral, reactimate, now )
+                                          , integral, reactimate, now, constant, event, tag, hold )
 import Graphics.Gloss                     ( Display (InWindow)
                                           , Picture (Color,Translate)
                                           , circleSolid
@@ -16,34 +16,54 @@ import qualified Graphics.Gloss.Interface.IO.Game as G
 
 type Pos = Double
 type Vel = Double
+type Acc = Double
 
-data Direction = Pressed | None
+data Direction = Up | Down deriving Show
+type GameInput = Event Direction
 
-fallingBall :: Pos -> SF a (Pos, Vel)
-fallingBall y0 = proc _ -> do
-  v <- integral -< -9.81
+acceleration :: SF Direction Acc
+acceleration = proc d -> case d of
+  Up -> constant 98 -< ()
+  _ -> constant (-98) -< ()
+
+fallingBall :: Pos -> SF Direction (Pos, Vel)
+fallingBall y0 = proc d -> do
+  a <- acceleration -< d
+  v <- integral -< a
   y <- integral -< v
   returnA -< (y + y0, v)
 
 drawBall :: Pos -> Picture
 drawBall p = Translate 0 (realToFrac p) $ circleSolid 10
 
-animateBall :: SF a Picture
-animateBall = proc _ -> do
-  (pos, vel) <- fallingBall 0 -< ()
+animateBall :: SF Direction Picture
+animateBall = proc i -> do
+  (pos, vel) <- fallingBall 0 -< i
   returnA -< drawBall pos
 
-parseInput :: SF (Event InputEvent) (Event Direction)
-parseInput = proc e -> case e of
-    Event (G.EventKey (G.SpecialKey G.KeyUp) G.Down _ _) -> now Pressed -< ()
-    _ -> now None -< ()
+parsegInput :: InputEvent -> Direction
+parsegInput (G.EventKey (G.SpecialKey G.KeyRight) G.Down _ _) = Up
+parsegInput _ = Down
+
+parseInput :: SF (Event InputEvent) GameInput
+parseInput = proc e ->
+  returnA -< fmap parsegInput e
+--parseInput = proc e -> case e of
+--    Event (G.EventKey (G.SpecialKey G.KeyUp) G.Down _ _) -> hold (Event Down) -< Event (Event Up)
+--    _ -> hold (Event Up) -< (Event (Event (Down)))
+--parseInput = proc e -> case e of
+--    Event (G.EventKey (G.SpecialKey G.KeyUp) G.Down _ _) -> now Up -< ()
+--    _ -> now Down -< ()
 
 defaultPlay :: SF (Event InputEvent) Picture -> IO ()
 defaultPlay = playYampa (InWindow "YampaDemo" (1280, 1050) (200, 200)) white 30
 
 main :: IO ()
-main = defaultPlay $ animateBall
---main = reactimate (return ())
---                  (\_ -> threadDelay 100000 >> return (0.1, Nothing))
+main = defaultPlay $ parseInput >>> (hold Down) >>> animateBall
+--main = reactimate (return $ Event (G.EventKey (G.SpecialKey G.KeyLeft) G.Down (G.Modifiers G.Up G.Up G.Up) (1,1)))
+--                  (\_ -> threadDelay 10000 >> return (0.1, Just (Event $ G.EventKey (G.SpecialKey G.KeyUp) G.Down (G.Modifiers G.Up G.Up G.Up) (1,1))))
+--                  (\_ -> return (0.1,  Nothing))
+--                  (\_ e -> putStrLn (show e) >> return False)
+--                  (parseInput)
 --                  (\_ (pos, vel) -> putStrLn ("pos: " ++ show pos ++ " vel: " ++ show vel) >> return False)
 --                  (fallingBall 10)
