c50fd8abc6d96cfefc8fd13590eb286a6493d75e
Brendan DeLeeuw <test>
2024-05-22 11:59:15 -0600

    Changing hard coded ball code into an example

--- 01_pong/app/Ball.hs
+++ 01_pong/app/Ball.hs
@@ -44,8 +44,17 @@ both = BothCollision
 
 data BallInput = BallInput {
   collision :: Event Collision,
-  score :: Event ()
+  reset :: Event ()
 } deriving (Eq, Show)
+
+type CollisionSF a = SF a (Event Collision)
+type Score a = SF a (Event ())
+
+v2x :: V2 a -> a
+v2x (V2 x _) = x
+
+v2y :: V2 a -> a
+v2y (V2 _ y) = y
 
 reflect :: Collision -> V2 Float -> V2 Float
 reflect VerticalCollision = (*) $ V2 1 (-1)
@@ -58,51 +67,44 @@ pongBall initial = switch ball pongBall
     ball = proc bi -> do
       v <- accumHold $ bV initial -< reflect <$> collision bi
       p <- integral -< v
-      s <- notYet -< score bi
+      s <- notYet -< reset bi
       let bs = initial {bP = p + bP initial}
       returnA -< (bs, s `tag` initial {bV = v})
 
-pongBallCollision :: SF BallState (Event Collision)
-pongBallCollision = proc bs -> do
-  vc <- edgeTag vc -< abs (y $ bP bs) >= 200
-  hc <- edgeTag hc -< abs (x $ bP bs) >= 200
-  let c = mergeBy col vc hc
+exampleBallCollision :: CollisionSF (a, BallState)
+exampleBallCollision = proc (_, bs) -> do
+  vc <- edgeTag vc -< abs (v2y $ bP bs) >= 200
+  hc <- edgeTag hc -< abs (v2x $ bP bs) >= 200
+  let c = mergeBy (const $ const both) vc hc
   returnA -< c
-  where
-    y (V2 _ y) = y
-    x (V2 x _) = x
-    col _ _ = both
 
-ball :: BallState -> SF BallInput BallState
-ball initial = proc bi -> do
+exampleBallScore :: Score a
+exampleBallScore = repeatedly 7 ()
+
+ball :: BallState -> CollisionSF (a, BallState) -> Score (a, BallState) -> SF a BallState
+ball initial c s = proc bi -> do
   rec
-    bs <- pongBall initial -< bi'
-    c <- pongBallCollision -< bs
-    s <- repeatedly 5 () -< bi
-    let bi' = BallInput c s
+    c <- c -< (bi, bs)
+    s <- s -< (bi, bs)
+    bs <- pongBall initial -< BallInput c s
   returnA -< bs
 
 drawBall :: BallState -> Picture
 drawBall (BallState (V2 x y)  _ color) = Color color $ Translate x y $ circleSolid 10
 
--- ballTest :: [SF BallInput BallState] -> SF [BallState] [BallState]
--- ballTest objs = dpSwitch routeTest objs never undefined
+exampleBall :: SF a BallState
+exampleBall = ball (BallState (V2 0 0) (V2 100 (-50)) black) exampleBallCollision exampleBallScore
 
--- routeTest :: [BallState] -> [sf] -> [(BallInput, sf)]
--- routeTest objs sfs = fmap ((,) BallInput) sfs
---   where
---     collisions = 
-
-animateBall :: SF a Picture
-animateBall = proc i -> do
-  bs <- ball (BallState (V2 0 0) (V2 100 50) black) -< (BallInput NoEvent NoEvent)
+animateBall :: SF a BallState ->  SF a Picture
+animateBall ball = proc i -> do
+  bs <- ball -< i
   returnA -< drawBall bs
 
 defaultPlay :: SF (Event InputEvent) Picture -> IO ()
 defaultPlay = playYampa (InWindow "YampaDemo" (1280, 1050) (200, 200)) white 30
 
 main :: IO ()
-main = defaultPlay $ animateBall
+main = defaultPlay $ animateBall exampleBall
 --main = reactimate (return $ Event (G.EventKey (G.SpecialKey G.KeyLeft) G.Down (G.Modifiers G.Up G.Up G.Up) (1,1)))
 --                  (\_ -> threadDelay 10000 >> return (0.1, Just (Event $ G.EventKey (G.SpecialKey G.KeyUp) G.Down (G.Modifiers G.Up G.Up G.Up) (1,1))))
 --                  (\_ -> return (0.1,  Nothing))
