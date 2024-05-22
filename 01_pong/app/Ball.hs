12649049d3ee685b6323fd340ee4bcce75f1e9d5
Brendan DeLeeuw <test>
2024-05-22 13:45:53 -0600

    Change ball collision to be bouncing off a generic surface vector

--- 01_pong/app/Ball.hs
+++ 01_pong/app/Ball.hs
@@ -3,18 +3,18 @@
 import Control.Concurrent
 import Control.Arrow                      ( returnA, (>>>), arr )
 import FRP.Yampa                          ( SF, Event (Event, NoEvent)
-                                          , VectorSpace(zeroVector, (*^), (^+^), dot)
+                                          , VectorSpace(zeroVector, (*^), (^+^), dot, (^/), norm)
                                           , integral, reactimate, now, notYet
                                           , constant, event, tag, hold, after
                                           , mapFilterE, isEvent, edge, lMerge
                                           , identity, accumHold, repeatedly, delayEvent
-                                          , dpSwitch, never, edgeTag, mergeBy, switch)
+                                          , dpSwitch, never, edgeTag, mergeBy, switch, parB)
 import Graphics.Gloss                     ( Display (InWindow)
                                           , Color
                                           , Picture (Color,Translate, Pictures)
-                                          , circleSolid, black, white, red )
+                                          , circleSolid, black, white, red, green, blue, orange, yellow)
 import Debug.Trace ( trace )
-import Linear.V2   ( V2 (V2), perp, ex, ey )
+import Linear.V2   ( V2 (V2), perp, unangle )
 import qualified Linear.Vector as Vector
 import qualified Linear.Metric as Metric
 import Graphics.Gloss.Interface.FRP.Yampa ( InputEvent, playYampa )
@@ -31,23 +31,20 @@ data BallState = BallState {
   bColor :: Color
 } deriving (Eq, Show)
 
-data Collision = VerticalCollision | HorizontalCollision | BothCollision deriving (Eq, Show)
-
-vc :: Collision
-vc = VerticalCollision
-
-hc :: Collision
-hc = HorizontalCollision
-
-both :: Collision
-both = BothCollision
-
 data BallInput = BallInput {
-  collision :: Event Collision,
-  reset :: Event ()
-} deriving (Eq, Show)
+  bBounce :: Event Bounce,
+  bReset :: Event ()
+}
 
-type CollisionSF a = SF a (Event Collision)
+type Bounce = V2 Float -> V2 Float
+
+vertical :: Bounce
+vertical v = bounce v (V2 0 1)
+
+horizontal :: Bounce
+horizontal v = bounce v (V2 1 0)
+
+type Collision a = SF a (Event Bounce)
 type Score a = SF a (Event ())
 
 v2x :: V2 a -> a
@@ -56,32 +53,36 @@ v2x (V2 x _) = x
 v2y :: V2 a -> a
 v2y (V2 _ y) = y
 
-reflect :: Collision -> V2 Float -> V2 Float
-reflect VerticalCollision = (*) $ V2 1 (-1)
-reflect HorizontalCollision = (*) $ V2 (-1) 1
-reflect _ = (*) (-1)
+-- Doesn't always work as expected when the surface
+-- vector is parallel or orthogonal to the vector
+-- in that case bounce twice
+-- TODO: can I change this function to do 2 bounces?
+bounce :: V2 Float -> V2 Float -> V2 Float
+bounce a s = (norm a / norm b) *^ b
+  where
+    b = a - (2 * (dot a s) *^ s)
 
 pongBall :: BallState -> SF BallInput BallState
 pongBall initial = switch ball pongBall
   where
     ball = proc bi -> do
-      v <- accumHold $ bV initial -< reflect <$> collision bi
+      v <- accumHold $ bV initial -< bBounce bi
       p <- integral -< v
-      s <- notYet -< reset bi
+      s <- notYet -< bReset bi
       let bs = initial {bP = p + bP initial}
       returnA -< (bs, s `tag` initial {bV = v})
 
-exampleBallCollision :: CollisionSF (a, BallState)
+exampleBallCollision :: Collision (a, BallState)
 exampleBallCollision = proc (_, bs) -> do
-  vc <- edgeTag vc -< abs (v2y $ bP bs) >= 200
-  hc <- edgeTag hc -< abs (v2x $ bP bs) >= 200
-  let c = mergeBy (const $ const both) vc hc
+  vc <- edgeTag vertical -< abs (v2y $ bP bs) >= 200
+  hc <- edgeTag horizontal -< abs (v2x $ bP bs) >= 200
+  let c = mergeBy (.) vc hc
   returnA -< c
 
 exampleBallScore :: Score a
 exampleBallScore = repeatedly 7 ()
 
-ball :: BallState -> CollisionSF (a, BallState) -> Score (a, BallState) -> SF a BallState
+ball :: BallState -> Collision (a, BallState) -> Score (a, BallState) -> SF a BallState
 ball initial c s = proc bi -> do
   rec
     c <- c -< (bi, bs)
@@ -92,8 +93,8 @@ ball initial c s = proc bi -> do
 drawBall :: BallState -> Picture
 drawBall (BallState (V2 x y)  _ color) = Color color $ Translate x y $ circleSolid 10
 
-exampleBall :: SF a BallState
-exampleBall = ball (BallState (V2 0 0) (V2 100 (-50)) black) exampleBallCollision exampleBallScore
+exampleBall :: Color -> V2 Float -> SF a BallState
+exampleBall c vi = ball (BallState (V2 0 0) vi c) exampleBallCollision exampleBallScore
 
 animateBall :: SF a BallState ->  SF a Picture
 animateBall ball = proc i -> do
@@ -104,7 +105,14 @@ defaultPlay :: SF (Event InputEvent) Picture -> IO ()
 defaultPlay = playYampa (InWindow "YampaDemo" (1280, 1050) (200, 200)) white 30
 
 main :: IO ()
-main = defaultPlay $ animateBall exampleBall
+main = defaultPlay $ parB (animateBall <$> [
+  exampleBall black (V2 50 (-50)),
+  exampleBall red (V2 50   50 ),
+  exampleBall blue (V2 50  100 ),
+  exampleBall green (V2 50 (-100)),
+  exampleBall orange (V2 0  (-100)),
+  exampleBall yellow (V2 50 0 )
+  ]) >>> arr Pictures
 --main = reactimate (return $ Event (G.EventKey (G.SpecialKey G.KeyLeft) G.Down (G.Modifiers G.Up G.Up G.Up) (1,1)))
 --                  (\_ -> threadDelay 10000 >> return (0.1, Just (Event $ G.EventKey (G.SpecialKey G.KeyUp) G.Down (G.Modifiers G.Up G.Up G.Up) (1,1))))
 --                  (\_ -> return (0.1,  Nothing))
