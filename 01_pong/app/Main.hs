4817dfd2cbc94f2467e75477e1017848c2464c8b
Brendan DeLeeuw <test>
2024-05-25 22:06:27 -0600

    hack in second paddle

--- 01_pong/app/Main.hs
+++ 01_pong/app/Main.hs
@@ -24,17 +24,32 @@ import Linear.GJK (minkCircle, minkRectangle)
 data GameInput = GameInput {
   keyUp :: G.KeyState,
   keyDown :: G.KeyState,
+  keyW :: G.KeyState,
+  keyS :: G.KeyState,
   screenSize :: V2 Int
 } deriving Show
 
-paddleInput :: SF (GameInput, PaddleState) PaddleInput
-paddleInput = proc (gi, ps) -> do
+data Score = SLeft | SRight deriving (Show, Eq)
+
+-- TODO: must be a better way to handle the input for 2 paddles
+paddle1Input :: SF (GameInput, PaddleState) PaddleInput
+paddle1Input = proc (gi, ps) -> do
   -- TODO: stop paddle when it reaches the top of the screen
   returnA -< PaddleInput $ d gi
   where
     d GameInput{keyUp = G.Down, keyDown = G.Up}   = PaddleUp
     d GameInput{keyUp = G.Down, keyDown = G.Down} = PaddleStop
     d GameInput{keyUp = G.Up, keyDown = G.Down}   = PaddleDown
+    d _ = PaddleStop
+
+paddle2Input :: SF (GameInput, PaddleState) PaddleInput
+paddle2Input = proc (gi, ps) -> do
+  -- TODO: stop paddle when it reaches the top of the screen
+  returnA -< PaddleInput $ d gi
+  where
+    d GameInput{keyW = G.Down, keyS = G.Up}   = PaddleUp
+    d GameInput{keyW = G.Down, keyS = G.Down} = PaddleStop
+    d GameInput{keyW = G.Up, keyS = G.Down}   = PaddleDown
     d _ = PaddleStop
 
 wallCollision :: SF (GameInput, BallState) (Event Bounce)
@@ -50,8 +65,10 @@ paddleCollision = proc (ps, bs) -> do
   let
     p = minkRectangle (pP ps) (pS ps)
     b = minkCircle 10 (bP bs)
-  c <- edgeTag horizontal -< fromMaybe False $ collision 10 p b
+  c <- edgeTag horizontal -< fromMaybe False (collision 10 p b) && infront bs ps
   returnA -< c
+  where
+    infront BallState{bP = (V2 b _)} PaddleState{pP = (V2 p _)} = abs b <= abs p
 
 score :: SF (GameInput, BallState) (Event ())
 score = proc (gi, bs) -> do
@@ -75,10 +92,13 @@ ballInput = proc ((gi, ps), bs) -> do
     merge = (fmap $ foldl1 (.)) . catEvents
 
 ball' :: SF (GameInput, PaddleState) BallState
-ball' = ball (BallState (V2 0 0) (V2 100 200) black) ballInput
+ball' = ball (BallState (V2 0 0) (V2 200 400) black) ballInput
 
-paddle' :: SF GameInput PaddleState
-paddle' = paddle (PaddleState (V2 200 0) 200 (V2 5 60) black) paddleInput
+paddle1 :: SF GameInput PaddleState
+paddle1 = paddle (PaddleState (V2 200 0) 200 (V2 5 60) black) paddle1Input
+
+paddle2 :: SF GameInput PaddleState
+paddle2 = paddle (PaddleState (V2 (-200) 0) 200 (V2 5 60) black) paddle2Input
 
 parseGameInput :: GameInput -> InputEvent -> GameInput
 -- parseGameInput gi i | trace ((show gi) ++ " " ++ show i) False = undefined
@@ -86,17 +106,25 @@ parseGameInput gi (G.EventKey (G.SpecialKey G.KeyUp) G.Down _ _)   = gi { keyUp 
 parseGameInput gi (G.EventKey (G.SpecialKey G.KeyUp) G.Up _ _)     = gi { keyUp = G.Up }
 parseGameInput gi (G.EventKey (G.SpecialKey G.KeyDown) G.Down _ _) = gi { keyDown = G.Down }
 parseGameInput gi (G.EventKey (G.SpecialKey G.KeyDown) G.Up _ _)   = gi { keyDown = G.Up }
+parseGameInput gi (G.EventKey (G.Char 'w') G.Down _ _)   = gi { keyW = G.Down }
+parseGameInput gi (G.EventKey (G.Char 'w') G.Up _ _)     = gi { keyW = G.Up }
+parseGameInput gi (G.EventKey (G.Char 's') G.Down _ _) = gi { keyS = G.Down }
+parseGameInput gi (G.EventKey (G.Char 's') G.Up _ _)   = gi { keyS = G.Up }
 parseGameInput gi (G.EventResize (x, y)) = gi {screenSize = V2 x y}
 parseGameInput gi _ = gi
 
 input :: SF (Event InputEvent) GameInput
-input = accumHoldBy parseGameInput $ GameInput G.Up G.Up (V2 100 100)
+input = accumHoldBy parseGameInput $ GameInput G.Up G.Up G.Up G.Up (V2 100 100)
 
 game :: SF GameInput Picture
 game = proc gi -> do
-  p <- paddle' -< gi
-  b <- ball' -< (gi, p)
-  returnA -< Pictures [(drawBall b), (drawPaddle p)]
+  p1 <- paddle1 -< gi
+  p2 <- paddle2 -< gi
+  rec
+    b <- ball' -< (gi, if (s $ bP b) then p1 else p2)
+  returnA -< Pictures [(drawBall b), (drawPaddle p1), (drawPaddle p2)]
+  where
+    s (V2 x _) = x > 0
 
 defaultPlay :: SF (Event InputEvent) Picture -> IO ()
 defaultPlay = playYampa (InWindow "YampaDemo" (500, 250) (200, 200)) white 60
