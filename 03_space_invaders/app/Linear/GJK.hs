fecfe0b13e536f1fa28bd8ae001876762e5ee0ca
Brendan DeLeeuw <mystiriodislykos@gmail.com>
2024-09-01 16:56:06 -0600

    starting to work on representing collision rectangles with a point, width, and height instead of many points

--- 03_space_invaders/app/Linear/GJK.hs
+++ 03_space_invaders/app/Linear/GJK.hs
@@ -1,20 +1,42 @@
-module Linear.GJK ( collision'
-                  , minkCircle
+module Linear.GJK ( minkCircle
                   , minkRectangle, minkRectangle'
                   , minkPoly
                   , minkSegment) where
 
 import Data.Maybe (fromMaybe)
 
-import GJK.Collision (collision)
 import GJK.Mink      (Mink)
-import GJK.Point     (Pt, dot)
+import GJK.Point     (Pt, dot, add)
 import GJK.Support   (polySupport)
 
 import Linear.V2 ( V2 (V2) )
 
-collision' :: (Mink a, Mink b) -> Bool
-collision' = (fromMaybe False) . uncurry (collision 10)
+type Center = V2 Double
+type Size = V2 Double
+type Rectangle = (Center, Size)
+
+len a = sqrt $ dot a a
+
+rectangleSupport' :: Size -> Pt -> Maybe Pt
+rectangleSupport' (V2 w h) (i, j)
+  | i == 0 = Just (0, sh)
+  | j == 0 = Just (sw, 0)
+  | i == 0 && j == 0 = Nothing
+  | otherwise =
+    let
+      i' = abs i
+      j' = abs j
+      p1 = (sw, w'/i' * j)
+      p2 = (h'/j' * i, sh)
+    in if len p1 < len p2 then Just p1 else Just p2
+  where
+    w' = w/2
+    h' = h/2
+    sw = w' * signum i
+    sh = h' * signum j
+
+rectangleSupport :: Rectangle -> Pt -> Maybe Pt
+rectangleSupport ((V2 x y), s) p = (add (x, y)) <$> rectangleSupport' s p
 
 circleSupport :: (Double, V2 Double) -> Pt -> Maybe Pt
 circleSupport (r, (V2 x y)) d@(a,b) =
